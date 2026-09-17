c0c98c6845de7979ecd8cbdc34b4deb9836baacb
Andre-Braga03 <yakuzo59@gmail.com>
2026-09-17 10:58:42 +0100

    generate pages recursively

--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+from markdown_blocks import generate_pages_recurvise
 
-def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
 
+def copy_static(source: str, dest: str) -> None:
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+
+    for name in os.listdir(source):
+        src_path = os.path.join(source, name)
+        dest_path = os.path.join(dest, name)
+        print(f"copying {src_path} -> {dest_path}")
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+        else:
+            copy_static(src_path, dest_path)
+
+def main() -> None:
+    dest = "public"
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    os.mkdir(dest)
+    copy_static("static", dest)
+    generate_pages_recurvise("content", "template.html", dest)
 
 
 if __name__ == "__main__":
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from htmlnode import ParentNode
 from textnode import TextNode, TextType, text_to_textnodes, text_node_to_html_node
+import os
 
 
 class BlockType(Enum):
@@ -127,3 +128,39 @@ def block_to_html_node(block: str) -> ParentNode:
     if block_type == BlockType.ORDERED_LIST:
         return ordered_list_to_html_node(block)
     raise ValueError(f"invalid block type: {block_type}")
+
+
+def extract_title(markdown: str) -> str:
+    if not markdown.startswith("#"):
+        raise ValueError("markdown does not start with a header")
+    
+    return markdown.split("\n")[0].strip("#").strip()
+
+def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+    print( f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r") as file:
+        markdown = file.read()
+    with open(template_path, "r") as file:
+        templace = file.read()
+    
+    htmlString = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+
+    page = templace.replace("{{ Title }}", title).replace("{{ Content }}", htmlString)
+    
+    if not os.path.exists(os.path.dirname(dest_path)):
+        os.makedirs(os.path.dirname(dest_path))
+   
+    with open(dest_path, "w") as dest:
+        dest.write(page)
+
+def generate_pages_recurvise(dir_path_content: str, template_path: str, dest_dir_path: str) -> None:
+    for name in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content,name)
+        dest_path = os.path.join(dest_dir_path,name)
+        if os.path.isfile(from_path):
+            if from_path.endswith(".md"):
+                dest_path = dest_path.replace(".md", ".html")
+                generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recurvise(from_path, template_path, dest_path)
